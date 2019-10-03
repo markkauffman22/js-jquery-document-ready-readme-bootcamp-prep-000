1d5645c47cdc92f44e0557b9c689fd7f4059bde3
Mark Kauffman <markkauffman22@gmail.com>
2019-10-03 14:22:35 +0000

    Automatically backed up by Learn

--- js/script.js
+++ js/script.js
@@ -5,7 +5,7 @@
 $(document).ready(function() {
     // code to be executed goes here
     
-   $("#text").append("this is so freaking cool.");
+   $("#text").append(". Coming from script.js. This is cool.");
 
     
 });
