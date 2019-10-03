396a8202761f10149d96a617ee2f52d5fde9260e
Mark Kauffman <markkauffman22@gmail.com>
2019-10-03 14:20:19 +0000

    Automatically backed up by Learn

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 // jQuery 'listens' for document to be fully loaded 
 // then executes this ...
+// note: Every time you see $, think jQuery.
 
 $(document).ready(function() {
     // code to be executed goes here
