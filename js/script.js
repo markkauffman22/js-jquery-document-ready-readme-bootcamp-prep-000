d1a3f0a405e04c440a54fa7c40d852af6261f562
Mark Kauffman <markkauffman22@gmail.com>
2019-10-03 14:28:07 +0000

    Done.

---
+++ js/script.js
@@ -0,0 +1,10 @@
+// jQuery 'listens' for document to be fully loaded 
+// then executes this ...
+// note: Every time you see $, think jQuery.
+
+$(document).ready(function() {
+    // code to be executed goes here
+    
+   $("#text").append(". Coming from script.js. This is cool.");
+
+});
